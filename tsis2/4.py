9ad8a81fb61859327619411e9e2169649569a992
ayazhan112 <86416379+ayazhan112@users.noreply.github.com>
2021-07-01 12:23:26 +0600

    Add files via upload

---
+++ tsis2/4.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        ans = 0 
+        max = ans 
+        for i in gain:
+            ans += i
+            if ans > max:
+                max = ans
+
+        return max
